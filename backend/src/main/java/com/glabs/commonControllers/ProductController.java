b58676aa8257887d26366b00c3eaed02f4335275
Bracerr <89707169+Bracerr@users.noreply.github.com>
2024-06-12 13:28:22 +0300

    add: switching and accessories product

--- backend/src/main/java/com/glabs/commonControllers/ProductController.java
+++ backend/src/main/java/com/glabs/commonControllers/ProductController.java
@@ -1,24 +1,37 @@
 package com.glabs.commonControllers;
 
 import com.glabs.commonService.ProductService;
+import com.glabs.entities.accessories.services.AccessoriesService;
+import com.glabs.entities.amplifiers.services.AmplifierService;
+import com.glabs.entities.cases.services.CaseService;
+import com.glabs.entities.guitars.services.GuitarService;
+import com.glabs.entities.pedals.services.PedalService;
+import com.glabs.entities.strings.services.StringService;
+import com.glabs.entities.switching.services.SwitchingService;
 import com.glabs.payload.request.AddProductRequest;
+import com.glabs.payload.response.ProductResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Работа с продуктами", description = "Позволяет добавить продукты в выбранную подкатегорию")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/products/")
 public class ProductController {
+
     private final ProductService productService;
+    private final AccessoriesService accessoriesService;
+    private final AmplifierService amplifierService;
+    private final CaseService caseService;
+    private final GuitarService guitarService;
+    private final PedalService pedalService;
+    private final StringService stringService;
+    private final SwitchingService switchingService;
 
     @Operation(
             summary = "Добавление продукта",
@@ -26,7 +39,7 @@ public class ProductController {
                     "В итоге название колекции должно совпадать с названием колекции в БД"
     )
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "404", description = "нет данной колекции"),
+            @ApiResponse(responseCode = "404", description = "нет данной колекции при вставке"),
             @ApiResponse(responseCode = "400", description = "Отсутсвует одно из полей продукта или запроса"),
             @ApiResponse(responseCode = "200", description = "OK - продукт добавлен")
     })
@@ -35,4 +48,28 @@ public class ProductController {
         return productService.addNewProduct(request);
     }
 
+    @GetMapping("accessories")
+    private ProductResponse getAllAccessories(){
+        return accessoriesService.getAllAccessories();
+    }
+
+    @GetMapping("amps")
+    private ProductResponse getAllAmplifier(){
+        return amplifierService.getAllAmp();
+    }
+
+    @GetMapping("cases")
+    private ProductResponse getAllCases() { return  caseService.getAllCase(); }
+
+    @GetMapping("guitars")
+    private ProductResponse getAllGuitars() { return guitarService.getAllGuitar(); }
+
+    @GetMapping("pedals")
+    private ProductResponse getAllPedals() { return pedalService.getAllPedals(); }
+
+    @GetMapping("strings")
+    private ProductResponse getAllStrings() { return stringService.getAllStrings(); }
+
+    @GetMapping("switching")
+    private ProductResponse getAllSwitching() { return switchingService.getAllSwitching(); }
 }
--- backend/src/main/java/com/glabs/entities/switching/services/SwitchingService.java
+++ backend/src/main/java/com/glabs/entities/switching/services/SwitchingService.java
@@ -1,23 +1,22 @@
-package com.glabs.entities.strings.services;
+package com.glabs.entities.switching.services;
 
 import com.glabs.commonService.ProductConfigService;
 import com.glabs.payload.response.ProductResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
-public class StringService {
+@RequiredArgsConstructor
+public class SwitchingService {
 
     private final ProductConfigService productService;
 
-    public ProductResponse getAllStrings(){
-        productService.setCollectionEnd("String");
+    public ProductResponse getAllSwitching(){
+        productService.setCollectionEnd("Switching");
         return ProductResponse.builder()
-                .category("string")
+                .category("switching")
                 .subCategories(productService.getSubcategories())
                 .brands(productService.getBrands())
                 .products(productService.getProductMap()).build();
     }
-
 }
